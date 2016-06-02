a2b8eede2595574ff14fc8d1ceb210ac1681b1b3
coded5282 <edwardjchen@gmail.com>
2016-06-02 19:55:29 -0400

    modified days

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // MODULE
-var weatherSite = angular.module('weatherSite', ['ngRoute', 'ngResource']); 
+var weatherSite = angular.module('weatherSite', ['ngRoute', 'ngResource']); // Setting up module for app
 
 // ROUTES
-weatherSite.config(function ($routeProvider) {
+weatherSite.config(function ($routeProvider) { // Using route provider to access the pages 
    
     $routeProvider
     
@@ -16,17 +16,22 @@ weatherSite.config(function ($routeProvider) {
         controller: 'forecastController'
     })
     
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController' 
+    })
+    
 });
 
 // SERVICES
-weatherSite.service('locationService', function() {
+weatherSite.service('locationService', function() { // service to bind city between pages 
     
     this.city = "New York, NY"; 
     
 });
 
 // CONTROLLERS 
-weatherSite.controller('homeController', ['$scope', 'locationService', function($scope, locationService) {
+weatherSite.controller('homeController', ['$scope', 'locationService', function($scope, locationService) { // controller to check for changes in city on homepage 
     
     $scope.city = locationService.city; 
     
@@ -34,18 +39,18 @@ weatherSite.controller('homeController', ['$scope', 'locationService', function(
        locationService.city = $scope.city;         
     });
     
-    
-    
-    
+        
 }]);
 
-weatherSite.controller('forecastController', ['$scope', '$resource', 'locationService', function($scope, $resource, locationService) {
+weatherSite.controller('forecastController', ['$scope', '$resource', '$routeParams', 'locationService', function($scope, $resource, $routeParams, locationService) { // weather controller
     
-    $scope.city = locationService.city; 
+    $scope.city = locationService.city;
+    
+    $scope.days = $routeParams.days || 2; 
     
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=7943d885a13d3ec6897530fc86829e63", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
     
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 }); 
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days }); 
     
     $scope.convertToFahrenheit = function(degK) {
         
