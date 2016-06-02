f70b4d3fb2f72dae05074a21db155c9b3b797763
coded5282 <edwardjchen@gmail.com>
2016-06-02 18:18:37 -0400

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 // MODULE
 var weatherSite = angular.module('weatherSite', ['ngRoute', 'ngResource']); 
 
+// ROUTES
+weatherSite.config(function ($routeProvider) {
+   
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
+    
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
+    
+});
+
 // CONTROLLERS 
 weatherSite.controller('homeController', ['$scope', function($scope) {
     
