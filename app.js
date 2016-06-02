45abd3e22b7378125452d50874abe9ef19279973
coded5282 <edwardjchen@gmail.com>
2016-06-02 17:57:51 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// MODULE
+var weatherSite = angular.module('weatherSite', ['ngRoute', 'ngResource']); 
+
+// CONTROLLERS 
+weatherSite.controller('homeController', ['$scope', function($scope) {
+    
+}]);
+
+weatherSite.controller('forecastController', ['$scope', function($scope) {
+    
+}]);
